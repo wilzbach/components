f9e5c75567419df2b0115d30d2c2919a35c09b94
3askaal <3askaal@gmail.com>
2018-09-19 13:36:48 +0200

    made it build successfully, dist size too big still

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,24 @@
 const fs = require('fs')
 
+// const data = [
+//   fs.readFileSync('./node_modules/bootstrap/scss/bootstrap.scss'),
+//   fs.readFileSync('./src/styles/index.scss')
+// ]
+
 module.exports = {
   css: {
     loaderOptions: {
       sass: {
-        data: fs.readFileSync('./node_modules/bootstrap/scss/bootstrap.scss'),
-        includePaths: ['./node_modules/bootstrap/scss/', './node-modules', './']
+        data: `
+          @import "~bootstrap/scss/bootstrap";
+          @import "./src/styles/index";
+        `,
+        includePaths: [
+          './',
+          './node-modules/',
+          './node_modules/bootstrap/scss/',
+          './src/styles/'
+        ]
       }
     }
   }
