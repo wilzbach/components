4071083ed21d52f1d13d9772ae3956c544288138
3askaal <3askaal@gmail.com>
2018-09-24 14:03:37 +0200

    progress

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>
+        <a-button type="primary">Button</a-button>
+        <a-button class="btn-3" type="primary" icon="ni ni-bag-17">Button</a-button>
+        <a-button class="btn-2" type="primary" icon="ni ni-bag-17"></a-button>
+        <div class="mb-3 mt-5">
+            <small class="text-uppercase font-weight-bold">Pick your size</small>
+        </div>
+
+        <a-button type="primary" size="sm">Small</a-button>
+        <a-button type="primary">Regular</a-button>
+        <a-button type="primary" size="lg">Large Button</a-button>
+        <div class="mb-3 mt-5">
+            <small class="text-uppercase font-weight-bold">Pick your color</small>
+        </div>
+
+        <a-button class="btn-1" type="primary">Primary</a-button>
+        <a-button class="btn-1" type="info">Info</a-button>
+        <a-button class="btn-1" type="success">Success</a-button>
+        <a-button class="btn-1" type="warning">Warning</a-button>
+        <a-button class="btn-1" type="danger">Danger</a-button>
+        <a-button class="btn-1" type="neutral">Neutral</a-button>
+
+        <div class="mb-3 mt-5">
+            <small class="text-uppercase font-weight-bold">Outline</small>
+        </div>
+
+        <a-button class="btn-1" outline type="primary">Outline Primary</a-button>
+        <a-button class="btn-1" outline type="info">Outline Info</a-button>
+        <a-button class="btn-1" outline type="success">Outline Success</a-button>
+        <a-button class="btn-1" outline type="warning">Outline Warning</a-button>
+        <a-button class="btn-1" outline type="danger">Outline Danger</a-button>
+        <a-button class="btn-1" outline type="neutral">Outline Neutral</a-button>
+        <!-- Button links -->
+        <div class="mb-3 mt-5">
+            <small class="text-uppercase font-weight-bold">Links</small>
+        </div>
+        <a href="#" class="btn btn-link text-default">Default</a>
+        <a href="#" class="btn btn-link text-primary">Primary</a>
+        <a href="#" class="btn btn-link text-info">Info</a>
+        <a href="#" class="btn btn-link text-success">Success</a>
+        <a href="#" class="btn btn-link text-warning">Warning</a>
+        <a href="#" class="btn btn-link text-danger">Danger</a>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,48 +1,12 @@
-import VueLazyload from 'vue-lazyload'
-import VueClipboard from 'vue-clipboard2'
-import { vueUse } from './utils/plugins'
+import Vue from 'vue'
+import Lib from './lib'
 
-// Styles
-// import './assets/vendor/font-awesome/css/font-awesome.css'
-// import './assets/vendor/nucleo/css/nucleo.css'
-// import './assets/vendor/gilroy/gilroy.css'
+import App from './App.vue'
 
-// Custom
-import './styles/index.scss'
+Vue.use(Lib)
 
-// Bootstrap
-import 'bootstrap/scss/bootstrap.scss'
+Vue.config.productionTip = false
 
-// Components
-import * as components from './components'
-import * as extraComponents from './extra-components'
-import * as directives from './directives'
-
-const VuePlugin = {
-  install: function (Vue) {
-    if (Vue._asyncy_vue_installed) {
-      return
-    }
-
-    Vue._asyncy_vue_installed = true
-
-    for (let item in components) {
-      Vue.use(components[item])
-    }
-
-    for (let item in extraComponents) {
-      Vue.use(extraComponents[item])
-    }
-
-    for (let item in directives) {
-      Vue.use(directives[item])
-    }
-
-    Vue.use(VueLazyload)
-    Vue.use(VueClipboard)
-  }
-}
-
-vueUse(VuePlugin)
-
-export default VuePlugin
+new Vue({
+  render: h => h(App)
+}).$mount('#app')
