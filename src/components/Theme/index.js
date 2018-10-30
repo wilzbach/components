42482869163d9ab88f609d4620ae85b8a46dc129
Jean Barriere <jean@barriere.io>
2018-10-30 12:17:53 +0100

    feat(storybook): add components to the storybook

--- src/components/Theme/index.js
+++ src/components/Theme/index.js
@@ -1,14 +1,20 @@
-import aAstronaut from './Astronaut.vue'
-import aComet from './Comet.vue'
-import aPlanet from './Planet.vue'
-import aRocket from './Rocket.vue'
+import aAsset from './Asset'
+import aAstronaut from './Astronaut'
+import aComet from './Comet'
+import aConstellation from './Constellation'
+import aPlanet from './Planet'
+import aRocket from './Rocket'
+import aStars from './Stars'
 import { registerComponents, vueUse } from '../../utils/plugins'
 
 const components = {
+  aAsset,
   aAstronaut,
   aComet,
+  aConstellation,
   aPlanet,
-  aRocket
+  aRocket,
+  aStars
 }
 
 const VuePlugin = {
--- stories/components/Theme/Theme.story.js
+++ stories/components/Theme/Theme.story.js
@@ -34,6 +34,31 @@ storiesOf('Components/', module).addWithJSX('Theme', () => ({
         <a-rocket size="lg" />
       </div>
 
+      <h3>Stars</h3>
+
+      <div class="bg--dark">
+        <a-stars />
+      </div>
+
+      <h3>Constellations (a, b, c)</h3>
+      <div class="bg--dark">
+        <a-constellation size="l" />
+        <a-constellation type="b" size="l" />
+        <a-constellation type="c" size="l" />
+      </div>
+
+      <h3>Misc assets</h3>
+      <div class="bg--dark">
+        <a-asset variant="book" height="10rem" />
+      </div>
+
+      <h3>Waves directive (v-waves)</h3>
+
+      <div class="bg--dark" style="padding: 200px 2rem 10px;">
+        <div style="width: 100%; height: 100px; padding: 0.5rem; border: 2px solid #efefef" class="text--light text--center" v-waves>Div with v-waves directive</div>
+      </div>
+
+
     </div>
     `
 }))
