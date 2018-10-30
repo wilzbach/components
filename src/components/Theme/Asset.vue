091467a2e784523987102501635e4b3e191a4432
Jean Barriere <jean@barriere.io>
2018-10-30 12:15:30 +0100

    feat(theme): add a-asset component with book variant

---
+++ src/components/Theme/Asset.vue
@@ -0,0 +1,45 @@
+<template>
+  <div
+    v-if="variant"
+    :class="['asset', variant]"
+    :style="{ width, height }" />
+</template>
+
+<script>
+export default {
+  name: 'Asset',
+  props: {
+    variant: {
+      type: String,
+      default: undefined,
+      description: 'The asset variant. (book)',
+      validator: v => ['book'].includes(v)
+    },
+    width: {
+      type: String,
+      default: undefined,
+      description: 'The asset width (inject inline style)'
+    },
+    height: {
+      type: String,
+      default: undefined,
+      description: 'The asset height (inject inline style)'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.asset {
+  display: block;
+  position: relative;
+  width: 4rem;
+  height: 4rem;
+  &.book {
+    background-image: url('../../assets/img/book.svg');
+    background-repeat: no-repeat;
+    background-size: contain;
+    background-position: center center;
+  }
+}
+</style>
