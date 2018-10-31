a3df331b09ee07ddcd8d850e8d7c20e913da3010
Jean Barriere <jean@barriere.io>
2018-10-31 15:31:40 +0100

    fix(grid): replace grid system by bulma grid system (auto-breakpoints and more responsive-friendly)

--- stories/components/Grid/Grid.story.js
+++ stories/components/Grid/Grid.story.js
@@ -5,19 +5,19 @@ storiesOf('Components/', module).addWithJSX('Grid', () => ({
     <div class="container">
       <h3 class="h3 font-weight-bold mb-4">Grid system</h3>
 
-      <div class="columns">
-        <div class="column one-half bg--cyan"> 1/2 </div>
-        <div class="column one-half bg--cyan"> 1/2 </div>
-        <div class="column one-third bg--cyan"> 1/3 </div>
-        <div class="column two-thirds bg--cyan"> 2/3 </div>
-        <div class="column one-quarter bg--cyan"> 1/4 </div>
-        <div class="column three-quarters bg--cyan"> 3/4 </div>
-        <div class="column one-fifth bg--cyan"> 1/5 </div>
-        <div class="column two-fifths bg--cyan"> 2/5 </div>
-        <div class="column three-fifths bg--cyan"> 3/5 </div>
-        <div class="column four-fifths bg--cyan"> 4/5 </div>
-        <div class="column one-sixth bg--cyan"> 1/6 </div>
-        <div class="column five-sixths bg--cyan"> 5/6 </div>
+      <div class="columns is-multiline is-mobile">
+        <div class="column is-half"><p class="bg--cyan">1/2</p></div>
+        <div class="column is-half"><p class="bg--cyan"> 1/2 </p></div>
+        <div class="column"><p class="bg--cyan"> 1/3 </p></div>
+        <div class="column is-two-thirds"><p class="bg--cyan"> 2/3 </p></div>
+        <div class="column is-one-quarter"><p class="bg--cyan"> 1/4 </p></div>
+        <div class="column is-one-quarter-tablet is-three-quarters-desktop is-three-quarters-widescreen is-three-quarters-fullhd"><p class="bg--cyan"> 3/4 </p></div>
+        <div class="column is-one-fifth"><p class="bg--cyan"> 1/5 </p></div>
+        <div class="column is-two-fifths"><p class="bg--cyan"> 2/5 </p></div>
+        <div class="column is-three-fifths"><p class="bg--cyan"> 3/5 </p></div>
+        <div class="column is-four-fifths"><p class="bg--cyan"> 4/5 </p></div>
+        <div class="column"><p class="bg--cyan"> 1/6 </p></div>
+        <div class="column"><p class="bg--cyan"> 5/6 </p></div>
       </div>
 
     </div>
